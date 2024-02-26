766292bbb00b8dfaac28dd375c2db48a8a00ea95
Rasmus Lauritsen <43439056+Lauritsen1@users.noreply.github.com>
2024-02-26 15:27:43 +0100

    sort names by len then alphabetical, add letter frequency count function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def main():
-    print("Yo")
+    names = ["Frank", "Alice", "Judy", "Bob", "Ivan", "Dave", "Heidi", "Charlie", "Eve", "Grace"]
+    names.sort(key=lambda n: (-len(n), n))
+    print(count_letter_frequency(names))
+
+def count_letter_frequency(names: list[str]):
+    letter_freq = {}
+    for n in names:
+        for l in n:
+            capitalized_letter = l.capitalize()
+            item = letter_freq.setdefault(capitalized_letter, 0)
+            letter_freq.update({capitalized_letter: item + 1})
+
+    return letter_freq
 
 main()
