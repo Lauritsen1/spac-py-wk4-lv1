3037c677bdaec4b885f8714ff1a4820fbaa668d7
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2024-02-27 20:39:37 +0100

    add wordcloud visualization of letter frequency and document code with comments

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from collections import defaultdict, Counter
 from matplotlib import pyplot as plt
+from wordcloud import WordCloud
 
 def main():
     names = ["Frank", "Alice", "Judy", "Bob", "Ivan", "Dave", "Heidi", "Charlie", "Eve", "Grace"]
+
+    # Sorts names by length (longest first), if same length, then alphabetically
     sorted_names = sorted(names, key=lambda n: (-len(n), n))
 
     letter_frequency = calculate_letter_frequency(sorted_names)
 
+    # Sorts dictionary by value in descending order
     items = sorted(letter_frequency.items(), key=lambda item: -item[1])
 
+    # Unpacks sorted items into two lists: x (letters) and y (frequencies)
     x, y = zip(*items)
 
-    plt.bar(x, y)
-    plt.xlabel('Letters')
-    plt.ylabel('Frequency')
-    plt.title('Frequency of each letter in list of names')
+    # Creates figure and plots
+    fig, (ax1, ax2) = plt.subplots(2)
+
+    # Bar chart
+    ax1.bar(x, y)
+    ax1.set_xlabel('Letters')
+    ax1.set_ylabel('Frequency')
+    ax1.set_title('Frequency of each letter in list of names')
+
+    # Wordcloud
+    wordCloud = WordCloud().generate_from_frequencies(letter_frequency)
+    ax2.imshow(wordCloud, interpolation="bilinear")
+    ax2.axis("off")
+
+    # Show figure
+    plt.tight_layout()
     plt.show()
 
-# First approach
+# 1. Calculates frequency of each letter in a list of names
 def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
     letter_frequency = defaultdict(int)
     for name in names:
@@ -26,7 +43,7 @@ def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
 
     return letter_frequency
 
-# Second/Simpler approach
+# 2. Calculates frequency of each letter in a list of names
 def calculate_letter_frequency(names: list[str]):
     letters = [letter.upper() for name in names for letter in name]
     return Counter(letters)
