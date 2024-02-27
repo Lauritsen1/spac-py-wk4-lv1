26099007b8ccdf5b427d5ece4580f35820162971
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2024-02-27 13:03:54 +0100

    add simpler calculate_letter_frequency func using Counter and list comprehension

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def main():
     names = ["Frank", "Alice", "Judy", "Bob", "Ivan", "Dave", "Heidi", "Charlie", "Eve", "Grace"]
     sorted_names = sorted(names, key=lambda n: (-len(n), n))
 
     letter_frequency = calculate_letter_frequency(sorted_names)
-    sorted_letter_frequency = dict(sorted(letter_frequency.items(), key=lambda item: -item[1]))
 
-    print(sorted_letter_frequency)
+    print(letter_frequency)
 
+# First approach
 def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
     letter_frequency = defaultdict(int)
     for name in names:
@@ -16,5 +16,10 @@ def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
             letter_frequency[letter] += 1
 
     return letter_frequency
+
+# Second/Simpler approach
+def calculate_letter_frequency(names: list[str]):
+    letters = [letter.upper() for name in names for letter in name]
+    return Counter(letters)
 
 main()
