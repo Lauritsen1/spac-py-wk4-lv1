58da1b14c3560aa50ab97572c53516b291cacef3
Rasmus Lauritsen <43439056+Lauritsen1@users.noreply.github.com>
2024-02-28 15:07:07 +0100

    add visualization for mean, median of name length

--- main.py
+++ main.py
@@ -1,50 +1,64 @@
 from collections import defaultdict, Counter
+from statistics import mean, median
 import random
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, gridspec
 from wordcloud import WordCloud
 from faker import Faker
 
 fake = Faker()
 
+
+
 def main():
     names = generate_name_list(10, 2)
-
-    # Sorts names by length (longest first), if same length, then alphabetically
     sorted_names = sorted(names, key=lambda n: (-len(n), n))
 
     letter_frequency = calculate_letter_frequency(sorted_names)
+    sorted_letter_frequency = sorted(letter_frequency.items(), key=lambda item: -item[1])
 
-    # Sorts dictionary by value in descending order
-    items = sorted(letter_frequency.items(), key=lambda item: -item[1])
+    fig = plt.figure(figsize=(10, 8))
 
-    # Unpacks sorted items into two lists: x (letters) and y (frequencies)
-    x, y = zip(*items)
+    gs = gridspec.GridSpec(2, 2, height_ratios=[1, 1]) 
 
-    # Creates figure and plots
-    fig, (ax1, ax2) = plt.subplots(2)
+    ax1 = plt.subplot(gs[0])
+    ax2 = plt.subplot(gs[2:])
+    ax3 = plt.subplot(gs[1])
 
-    # Bar chart
+    plt.subplots_adjust(hspace = 0.5)
+
+    x, y = zip(*sorted_letter_frequency)
+
     ax1.bar(x, y)
-    ax1.set_xlabel('Letters')
-    ax1.set_ylabel('Frequency')
-    ax1.set_title('Frequency of each letter in list of names')
+    ax1.set_ylabel("Frequency")
+    ax1.set_title("Frequency of each letter in list of names")
 
-    # Wordcloud
     wordCloud = WordCloud().generate_from_frequencies(letter_frequency)
+
     ax2.imshow(wordCloud, interpolation="bilinear")
     ax2.axis("off")
 
-    # Show figure
-    plt.tight_layout()
+    name_len = list(map(lambda n: len(n), names))
+    mean_len = mean(name_len)
+    median_len = median(name_len)
+
+    ax3.bar(names, name_len)
+    ax3.axhline(mean_len, color="red", linestyle="--", label="Average")
+    ax3.axhline(median_len, color="blue", linestyle="--", label="Median")
+    ax3.legend()
+    ax3.set_ylabel("Lenght")
+    ax3.set_title("Lenght of names")
+
+    plt.xticks(rotation = 90)
+  
     plt.show()
 
 def generate_name_list(total_amount: int, duplicates: int) -> list[str]:
     names = []
 
-    if duplicates < 0: # Returns list of unique names
+    if duplicates <= 0: # Returns list of unique names
         names = [fake.unique.name() for _ in range(total_amount)]
     elif duplicates > 0: # Returns list of names containing duplicates
-        names = [fake.name() for _ in range(total_amount - duplicates)]
+        names = [fake.first_name() for _ in range(total_amount - duplicates)]
         names.extend(random.choices(names, k = duplicates))
         random.shuffle(names)
 
