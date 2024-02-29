53132ee63e6935f7228b8b66862ce708373e551f
Rasmus Lauritsen <rasmus.lauritsen.dev@outlook.com>
2024-02-29 21:03:52 +0100

    organize and remove unnecessary code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from collections import defaultdict, Counter
-from statistics import mean, median
-import random
 from matplotlib import pyplot as plt, gridspec
 from wordcloud import WordCloud
 from faker import Faker
+
+from collections import Counter
+from statistics import mean, median
+import random
 
 fake = Faker()
 
@@ -13,8 +14,6 @@ def main():
     sorted_unique_names = sorted(unique_names, key=lambda n: (-len(n), n))
 
     letter_frequency = calculate_letter_frequency(sorted_unique_names)
-    sorted_letter_frequency = sorted(letter_frequency.items(), key=lambda item: -item[1])
-
     print(letter_frequency)
 
     fig = plt.figure(figsize=(10, 8))
@@ -27,7 +26,7 @@ def main():
 
     plt.subplots_adjust(hspace = 0.5)
 
-    x, y = zip(*sorted_letter_frequency)
+    x, y = zip(*letter_frequency.items())
 
     ax1.bar(x, y)
     ax1.set_title("Frequency of each letter in list of names")
@@ -54,23 +53,13 @@ def generate_name_list(total_amount: int, duplicates: int) -> list[str]:
 
     if duplicates <= 0: # Returns list of unique names
         names = [fake.unique.name() for _ in range(total_amount)]
-    elif duplicates > 0: # Returns list of names containing duplicates
+    elif duplicates > 0: # Returns list of names with duplicates
         names = [fake.first_name() for _ in range(total_amount - duplicates)]
         names.extend(random.choices(names, k = duplicates))
         random.shuffle(names)
 
     return names
 
-# 1. Calculates frequency of each letter in a list of names
-def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
-    letter_frequency = defaultdict(int)
-    for name in names:
-        for letter in name.upper():
-            letter_frequency[letter] += 1
-
-    return letter_frequency
-
-# 2. Calculates frequency of each letter in a list of names
 def calculate_letter_frequency(names: list[str]) -> dict[str, int]:
     letters = [letter.upper() for name in names for letter in name]
     return Counter(letters)
